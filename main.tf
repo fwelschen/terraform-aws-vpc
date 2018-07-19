1f6d184b58dd0b83984ff63938b2dec5b4483064
Martin Garibaldi <martin.garibaldi@trocafone.com>
2018-07-19 18:01:42 -0300

    Fix wrong number of subnets for route creation
    I have to evaluate the maximum value between private and backend subnets, to create the propper number of private route tables

--- main.tf
+++ main.tf
@@ -368,7 +368,7 @@ resource "aws_vpn_connection" "this" {
 # VPN private route propagation
 ###############################
 resource "aws_vpn_gateway_route_propagation" "private" {
-  count          = "${var.enable_vpn_route_private_propagation ? length(var.private_subnets) : 0}"
+  count          = "${var.enable_vpn_route_private_propagation ? max(length(var.private_subnets), length(var.backend_subnets)) : 0}"
   vpn_gateway_id = "${aws_vpn_gateway.this.id}"
   route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
 }
